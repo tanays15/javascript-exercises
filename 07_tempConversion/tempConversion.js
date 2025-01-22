537562eb17ccb8240f938873960be5cb0c407039
Tanay Sahasrabudhe <tanay.sahasrabudhe@gmail.com>
2025-01-21 22:13:49 -0500

    solved js exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let newTemp = Math.round(((temp - 32) * 5 / 9) * 10) / 10;
+  return newTemp % 1 == 0 ? Math.floor(newTemp) : newTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  newTemp = Math.round((temp * 9 / 5 + 32) * 10) / 10;
+  return newTemp % 1 == 0 ? Math.floor(newTemp) : newTemp;
 };
 
 // Do not edit below this line
