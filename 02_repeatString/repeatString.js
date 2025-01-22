f16ffec71d18be5c567b834ab767f94eb2fd6807
Tanay Sahasrabudhe <tanay.sahasrabudhe@gmail.com>
2025-01-21 21:39:49 -0500

    solved 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, n) {
+  if (n < 0) {
+    return "ERROR"
+  }
+  let res = "";
+  for (let i = 0; i < n; i++) {
+    res += string;
+  }
+  return res;
 };
 
 // Do not edit below this line
