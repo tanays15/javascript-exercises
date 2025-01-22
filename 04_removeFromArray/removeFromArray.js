8fe30e6f3f462fdf51cea5ddafd90a0ba38863e5
Tanay Sahasrabudhe <tanay.sahasrabudhe@gmail.com>
2025-01-21 21:51:38 -0500

    more js way to solve 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,10 @@
-const removeFromArray = function() {
-  let arr = arguments[0];
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 1; j < arguments.length; j++) {
-      if (arr[i] === arguments[j]) {
-        arr.splice(i, 1);
-        i -= 1;
-      }
+const removeFromArray = function(array, ...args) {
+  let arr = [];
+  array.forEach((item) => {
+    if (!args.includes(item)) {
+      arr.push(item);
     }
-  }
+  })
   return arr;
 };
 
