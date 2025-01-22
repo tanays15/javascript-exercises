ec99e7ff2c98ae16bf35cb959fe0d51e298ab117
Tanay Sahasrabudhe <tanay.sahasrabudhe@gmail.com>
2025-01-21 21:49:34 -0500

    solved javascript exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
 const removeFromArray = function() {
+  let arr = arguments[0];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 1; j < arguments.length; j++) {
+      if (arr[i] === arguments[j]) {
+        arr.splice(i, 1);
+        i -= 1;
+      }
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
