13aff859aa748e6c0881f33c89d17b2c9a6b715f
Tanay Sahasrabudhe <tanay.sahasrabudhe@gmail.com>
2025-01-21 21:58:33 -0500

    solved js exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(lower, upper) {
+  if (!Number.isInteger(lower) || !Number.isInteger(upper) || lower < 0 || upper < 0) {
+    return "ERROR"
+  }
+  let sum = 0;
+  if (lower > upper) {
+    let tmp = lower;
+    lower = upper;
+    upper = tmp;
+  }
+  while (lower <= upper) {
+    sum += lower;
+    lower++;
+  }
+  return sum;
 };
 
 // Do not edit below this line
