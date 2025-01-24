835c5e0d340120162e57ea936f5419c78c5a0095
Tanay Sahasrabudhe <tanay.sahasrabudhe@gmail.com>
2025-01-24 11:01:34 -0500

    solved js exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+  return arr.reduce((oldest, person) => {
+    if (person.yearOfDeath === undefined || oldest.yearOfDeath === undefined) {
+      return oldest = person.yearOfBirth > oldest.yearOfBirth ? oldest : person
+    } else {
+      const old = oldest.yearOfDeath - oldest.yearOfBirth;
+      const per = person.yearOfDeath - person.yearOfBirth;
+      return oldest = old > per ? oldest : person;
+    }
+  });
 };
 
 // Do not edit below this line
