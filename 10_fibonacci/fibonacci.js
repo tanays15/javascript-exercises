0692e065f9b9eabe40744bb0c6e083620c0b6290
Tanay Sahasrabudhe <tanay.sahasrabudhe@gmail.com>
2025-01-24 10:46:31 -0500

    solution for js exercise 09

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  if (n < 0) {
+    return "OOPS"
+  }
+  if (n == 0) {
+    return 0;
+  }
+  if (n < 3) {
+    return 1;
+  }
+  let first = 1;
+  let second = 1;
+  for (let i = 2; i < n; i++) {
+    let temp = second;
+    second = first + second;
+    first = temp;
+  }
+  return second;
 };
 
 // Do not edit below this line
