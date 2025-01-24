41cced75bb525f2d6625f7f164d33bf069fb15d2
Tanay Sahasrabudhe <tanay.sahasrabudhe@gmail.com>
2025-01-24 10:33:14 -0500

    solutions for js exercises 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+  return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  const sum = arr.reduce((total, currItem) => {
+    return total + currItem
+  }, 0);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  const prod = arr.reduce((total, currItem) => {
+    return total * currItem;
+  }, 1)
+  return prod;
 };
 
-const power = function() {
-	
+const power = function(n1, n2) {
+  let res = 1;
+  for (let i = 0; i < n2; i++) {
+    res *= n1;
+  }
+  return res;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let res = 1;
+  for (let i = 1; i <= n; i++) {
+    res *= i;
+  }
+  return res;
 };
 
 // Do not edit below this line
