8a4cf180394a6a9bba9a44d348b7bba95a676e8e
Tanay Sahasrabudhe <tanay.sahasrabudhe@gmail.com>
2025-01-24 10:43:05 -0500

    solution for js exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function(string) {
+  const alphanum = "abcdefghijklmnopqrstuvwxyz0123456789";
+  const newStr = string
+    .toLowerCase()
+    .split("")
+    .filter((char) => alphanum.includes(char))
+    .join("");
+  return newStr === newStr.split("").reverse().join("");
 };
 
 // Do not edit below this line
